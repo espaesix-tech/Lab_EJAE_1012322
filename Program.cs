e44b328c3210ebca5918ca635a12b3098ae37e7e
espaesix-tech <espaesix@gmail.com>
2026-02-17 20:22:06 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese nuevo modelo de nave:");
+        string modelo = Console.ReadLine();
+        Console.WriteLine("Ingrese Capacidad de carga:");
+        int Capacidad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Combustible actual:");
+        float Combustible = float.Parse(Console.ReadLine());
+        Console.WriteLine("¿Motor de salto activo?(true/false):");
+        bool Salto = bool.Parse(Console.ReadLine());
+        string estadoMotor = Salto ? "SI" : "NO";
+        Console.WriteLine("Modelo " + modelo + " Agregado");
+        Console.WriteLine("Capacidad de almacenamiento " + Capacidad + " Teragramos totales");
+        Console.WriteLine("El tanque de combustible se encuentra a: " + Combustible + " Litros actuales");
+        Console.WriteLine("Motor de salto, " + estadoMotor + " se encuentra el linea");
+    }
+}
+
